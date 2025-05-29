e50aa52a0048a8467a7a9e998f37d611a85def9c
GermanJavierR <german.j.r1002@gmail.com>
2025-05-28 21:29:54 -0300

    subiendo archivos

--- validaciones.py
+++ validaciones.py
@@ -7,6 +7,19 @@ def es_entero(valor):
         # for i in range(1, len(valor)):
         #     resto_del_valor = i 
         return valor.isdigit()
+    else:
+        return False
+
+
+# def es_negativo(valor):
+#     if isinstance(valor, int):
+#         return True
+#     elif type(valor) == str:
+#         primera_parte = valor[0]
+#         # resto_del_valor = ""
+#         # for i in range(1, len(valor)):
+#         #     resto_del_valor = i 
+
 
 
 def es_nota_valida(numero):
